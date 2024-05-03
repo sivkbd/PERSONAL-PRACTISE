e52adb46494a00058d5471b4ab21c02b463fa3b3
sivkbd <siviwe.kibido@gmail.com>
2024-05-04 00:42:22 +0200

    first commit

---
+++ PYTHON-PROJECTS/notes.py
@@ -0,0 +1,54 @@
+#print("hello world")
+#if statement
+#if 6> 3:
+    #print("six is greater than 3")
+
+#assigning to a variable
+# x =2
+# y = "hello"
+# print(x)
+#WHEN YOU SPECIFY THE DATA TYPE ON PYTHON,  USE CASTING
+#FOR EXAMPLE
+
+# x = int(4)
+# y = float(20)
+# z = str("john")
+
+# print(x, z, y)
+# print(y)
+# print(z)
+#we can also get the data type by using type function.
+# for example
+# a = 45
+# b = 49,3
+# c = "siviwe"
+
+# print(type(a),type (b),type (c))
+
+
+#upacking a collection in a list
+
+# fruits = ["apple", "banana", "lemon"]
+# e,r,t = fruits
+# print(e,r,t)
+
+#global variables, when variables created outside of a function 
+# but can be used both inside of function and outside 
+
+# name = "siviwe"
+# def myName():
+#     print(" my name is "+ name)
+# myName()
+
+#what happens if the global variable is the same as the variable inside the function.
+# surname = "kibido"
+
+# def mySurname():
+#     surname = " citywayo"
+#     print("my surname is"+ surname)
+
+# mySurname()
+
+# print("my surname "+ surname)
+
+
